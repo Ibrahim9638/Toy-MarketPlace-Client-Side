bc3d70b1e6b0833eb182862543bff686d9a9f527
Ibrahim <you@example.com>
2023-05-19 06:13:19 +0600

    local env setup

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logImg from '../../assets/login/login.jpg'
 
 const Login = () => {
+    const handleLogin = event=>{
+        event.preventDefault()
+    }
     return (
         <div>
-            <h1>This is a login Page</h1>
+             <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row ">
+          <div className="lg:w-full mr-14">
+            <img className='rounded' src={logImg} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <h1 className="text-3xl text-center font-bold">Login</h1>
+              <form onSubmit={handleLogin}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Login"
+                  />
+                </div>
+              </form>
+              <p className="text-center">You do not have an account? <Link to='/register' className='text-[#c22373] font-bold' >Register</Link> </p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import regImg from '../../assets/login/login.jpg'
+
+const Register = () => {
+  const handleSignUpForm =event =>{
+    event.preventDefault()
+  }
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row ">
+          <div className="w-full mr-14">
+            <img className='rounded' src={regImg} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <h1 className="text-3xl text-center font-bold">Register</h1>
+              <form onSubmit={handleSignUpForm}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Photo URL</span>
+                  </label>
+                  <input
+                    type="link"
+                    placeholder="Photo URL"
+                    name="photo"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Register"
+                  />
+                </div>
+              </form>
+              <p className="text-center">Have you already an account? <Link to='/login' className='text-[#c22373] font-bold' >Login</Link> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
